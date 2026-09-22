41c4018cb497fdda29b992f7f010fe560cce9494
Samuel Yam <2603176@sit.singaporetech.edu.sg>
2026-09-22 14:21:58 +0800

    Added Logic Manager for 6, _ branch on Main. Also prepped JSON branches, 2 and 3, for embryonic data manager development

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import os
 import json
 
-from io_manager import get_valid_other_industry, retrieve_company_industry, retrieve_company_name, retrieve_total_revenue
+from io_manager import retrieve_company_industry, retrieve_company_name, retrieve_total_revenue
 
 # Code
 print(
@@ -15,26 +15,27 @@ print(
 * ~  SME Green Sutainability Eligibility Scheme  ~ *
 -------------------------------------------------- *
 *                                                  *
+*    For the purposes of determining eligibility   *
+*  to the SME Green Grant and assessing compliance *
 *  WARNING! THIS APP IS PRIMITIVE AND MAY NOT WORK *
 *                                                  *
 ****************************************************
 ''')
 input("Press enter to continue ")
-scheme_grant_list = []
+scheme_grant_records = []
+ai_manager_data = []
 key = 1
 
 while key!=0:
     keyverify = False
     while keyverify == False:
-        key = input("Please select your choice (0,1,2,3,4)\n 0. Exit\n 1. Start\n 2. B\n 3. C\n 4. D\n")
+        key = input("Please select your choice (0,1,2,3,4,5,6)\n 0. Exit\n 1. Start\n 2. Save to JSON\n 3. Pull from JSON\n 4. Display Current Records\n 5. AI Processor\n 6. Logic Manager\n")
         if len(key) == 0:
             print("Empty Response!")
         elif key.isnumeric() == False:
             print("Error! Letters detected!!")
         elif key.isspace():
             print("Error! Space only answer not allowed")
-        elif int(key) < 0 or int(key) > 4:
-            print("Error! Unrecognised number option")
         else:
             keyverify = True
             key = int(key)
@@ -50,16 +51,24 @@ while key!=0:
             print(f"Company Name: {company_name}")
             print(f"Company Industry: {company_industry}")
             print(f"Company Total Revenue: {get_total_revenue}")
-            scheme_grant_list.append({
+            scheme_grant_records.append({
                 "Company Name": company_name,
                 "Company Industry": company_industry,
                 "Company Total Revenue": get_total_revenue
             })
-            print("Scheme Grant List:", scheme_grant_list)
-            print(type(scheme_grant_list))
-        case 2:
+            print("Scheme Grant Records:", scheme_grant_records)
+            print(type(scheme_grant_records))
+        case 2: #Save to JSON
             print("Function U/C")
-        case 3:
+            print(type(scheme_grant_records))
+        case 3: #Pull from JSON
             print("Function U/C")
-        case 4:
+            # scheme_grant_records = json.loads(scheme_grant_records)
+        case 4: #Display current scheme grant records
             print("Function U/C")
+        case 5: #AI Processor
+            print("Function U/C")
+        case 6: # For the Logic Manager
+            print("Function U/C")
+        case _:
+            print("Error! Unrecognised number option")
