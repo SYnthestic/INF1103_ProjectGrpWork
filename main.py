bbfba144cfa17e147e7221e7cb5da0287f2eb19e
Samuel Yam <2603176@sit.singaporetech.edu.sg>
2026-09-22 13:56:59 +0800

    Tested I/O functions. Looks good. For now, I'm just going to integrate IO with main

---
+++ main.py
@@ -0,0 +1,65 @@
+# Importations
+import sys
+import os
+import json
+
+from io_manager import get_valid_other_industry, retrieve_company_industry, retrieve_company_name, retrieve_total_revenue
+
+# Code
+print(
+'''
+****************************************************
+*                                                  *
+*                   Welcome to:                    *
+*                                                  *
+* ~  SME Green Sutainability Eligibility Scheme  ~ *
+-------------------------------------------------- *
+*                                                  *
+*  WARNING! THIS APP IS PRIMITIVE AND MAY NOT WORK *
+*                                                  *
+****************************************************
+''')
+input("Press enter to continue ")
+scheme_grant_list = []
+key = 1
+
+while key!=0:
+    keyverify = False
+    while keyverify == False:
+        key = input("Please select your choice (0,1,2,3,4)\n 0. Exit\n 1. Start\n 2. B\n 3. C\n 4. D\n")
+        if len(key) == 0:
+            print("Empty Response!")
+        elif key.isnumeric() == False:
+            print("Error! Letters detected!!")
+        elif key.isspace():
+            print("Error! Space only answer not allowed")
+        elif int(key) < 0 or int(key) > 4:
+            print("Error! Unrecognised number option")
+        else:
+            keyverify = True
+            key = int(key)
+
+    match key:
+        case 0:
+            print("Exiting the program.")
+        case 1: # Start checks
+            print("Function U/C")
+            company_name = retrieve_company_name()
+            company_industry = retrieve_company_industry()
+            get_total_revenue = retrieve_total_revenue()
+            print(f"Company Name: {company_name}")
+            print(f"Company Industry: {company_industry}")
+            print(f"Company Total Revenue: {get_total_revenue}")
+            scheme_grant_list.append({
+                "Company Name": company_name,
+                "Company Industry": company_industry,
+                "Company Total Revenue": get_total_revenue
+            })
+            print("Scheme Grant List:", scheme_grant_list)
+            print(type(scheme_grant_list))
+        case 2:
+            print("Function U/C")
+        case 3:
+            print("Function U/C")
+        case 4:
+            print("Function U/C")
