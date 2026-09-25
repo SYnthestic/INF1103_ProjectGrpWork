6e807bd6b453a158be241c5cc79573cbf5505c31
Samuel Yam <2603176@sit.singaporetech.edu.sg>
2026-09-25 11:08:38 +0800

    Setting up print to intro Logic Manager. Now construction can start

---
+++ logic_manager.py
@@ -0,0 +1,7 @@
+print('''This is the Logic Manager module. \n 
+It is responsible for handling the core logic of the application, \n
+ including data processing, calculations, and decision-making based on user inputs and stored data.\n
+The Logic Manager interacts with other modules such as the IO Manager for input/output operations, \n
+carries out arithmetic and logic operations after the AI Manager has handed over data to it \n
+And then pumps down the data to the Data Manager to store and the IO Manager to output
+''')
